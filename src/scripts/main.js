275615717d382a11520e6dc8e19a17715b7a0ab0
Daniel Clarke <deliotclarke@gmail.com>
2019-04-29 14:53:52 -0500

    added more to the database, including colors and added an input field to the html to accept user input of legos:

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 console.log("hello god? are you there?");
+
+fetch("http://localhost:8088/legos")
+  .then(response => response.json())
+  .then(legoObj => {
+    console.log(legoObj.color);
+  })
+
